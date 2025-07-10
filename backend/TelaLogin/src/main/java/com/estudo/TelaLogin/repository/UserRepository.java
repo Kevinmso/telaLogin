283a977c6a30ca47515c9d90453178c008836d8c
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-07-10 12:51:10 -0300

    Torna UserRepository uma interface que extende JpaRepository

--- backend/TelaLogin/src/main/java/com/estudo/TelaLogin/repository/UserRepository.java
+++ backend/TelaLogin/src/main/java/com/estudo/TelaLogin/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.estudo.TelaLogin.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository {
 }
