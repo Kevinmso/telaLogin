5cd9694fbf5e030581eba02f8883e9e28d196295
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-07-10 11:32:27 -0300

    Criar estrutura básica do backend

---
+++ backend/TelaLogin/src/test/java/com/estudo/TelaLogin/TelaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.TelaLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
