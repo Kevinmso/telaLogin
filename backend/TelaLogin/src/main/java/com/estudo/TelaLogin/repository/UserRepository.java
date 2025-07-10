f46282df31e3a2390728e07fab83c02aeb0f9fc7
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-07-10 17:27:21 -0300

    Criar métodos login e register em AuthService

--- backend/TelaLogin/src/main/java/com/estudo/TelaLogin/repository/UserRepository.java
+++ backend/TelaLogin/src/main/java/com/estudo/TelaLogin/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.estudo.TelaLogin.repository;
 
+import com.estudo.TelaLogin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
 }
--- backend/TelaLogin/src/main/java/com/estudo/TelaLogin/service/AuthService.java
+++ backend/TelaLogin/src/main/java/com/estudo/TelaLogin/service/AuthService.java
@@ -1,4 +1,37 @@
 package com.estudo.TelaLogin.service;
 
+import com.estudo.TelaLogin.model.User;
+import com.estudo.TelaLogin.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String register(User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+
+        if (existingUser.isPresent()) {
+            return "Nome de usuário já está em uso.";
+        }
+
+        userRepository.save(user);
+        return "Usuário registrado com sucesso!";
+    }
+
+    public String login(User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+
+        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+            return "Login bem-sucedido!";
+        }
+
+        return "Usuário ou senha inválidos.";
+    }
 }
